2f2d318c24af84d9db5764146960e51ee81a2efc
João Inez <joaoinez.info@gmail.com>
2026-05-03 21:13:01 +0200

    feat(cors): get cors origins from env file

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,22 +3,20 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.dependencies import lifespan
+from app.dependencies import get_settings, lifespan
 from app.docs import docs_router
 from app.url.main import url_router
+
+settings = get_settings()
 
 app = FastAPI(
     lifespan=lifespan,
     openapi_prefix="/api" if os.environ.get("APP_ENV") == "production" else "",
 )
 
-origins = [
-    "http://localhost",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=settings.cors_origins_list,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- backend/app/settings.py
+++ backend/app/settings.py
@@ -5,6 +5,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     db_url: str
+    cors_origins: str
+
+    @property
+    def cors_origins_list(self) -> list[str]:
+        return [origin.strip() for origin in self.cors_origins.split(",")]
 
     model_config = SettingsConfigDict(  # pyright: ignore[reportUnannotatedClassAttribute]
         env_file=".env"
